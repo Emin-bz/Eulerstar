1158f1f9f00fca0328a6503ba8005fef504540c0
Emin Boz <emb@Emins-MacBook-Air.local>
2023-02-12 02:18:00 +0100

    Switch from open price to close price usage

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import warnings
 warnings.filterwarnings('ignore')
 
 _start = "2022-11-01"
-_end = "2023-02-11"
+_end = "2023-02-13"
 
 data = fetch.load(_start, _end, 'polygon', 50000)
 
@@ -56,15 +56,15 @@ def detect_w_pattern_second_bottom(p):
   
 trace = 0
 
-for idx in range(20, len(data['Open'])):
-  if detect_w_pattern_second_bottom(data['Open'][trace:idx]):
-    opened_at = (data['Datetime'][idx] if data['type'][idx] == 'polygon' else data.index[idx], data['Open'][idx])
+for idx in range(20, len(data['Close'])):
+  if detect_w_pattern_second_bottom(data['Close'][trace:idx]):
+    opened_at = (data['Datetime'][idx] if data['type'][idx] == 'polygon' else data.index[idx], data['Close'][idx])
 
   elif opened_at != None:
-    if data['Open'][idx] >= opened_at[1] * 1.004:
-      res = f"Opened: {opened_at[0]}, {opened_at[1]}, Closed: {data['Datetime'][idx] if data['type'][idx] == 'polygon' else data.index[idx]} {data['Open'][idx]}"
+    if data['Close'][idx] >= opened_at[1] * 1.004:
+      res = f"Opened: {opened_at[0]}, {opened_at[1]}, Closed: {data['Datetime'][idx] if data['type'][idx] == 'polygon' else data.index[idx]} {data['Close'][idx]}"
       print(res)
-      calc_profit(opened_at[1], data['Open'][idx])
+      calc_profit(opened_at[1], data['Close'][idx])
       opened_at = None
   
   trace += 1
@@ -74,4 +74,4 @@ if opened_at != None:
   print(res)
 
 print('\n')
-print(f'Total profit ({_start} - {_end}):', profit, '$ USD')
+print(f'Total profit ({_start}-{_end}): {profit}$ USD ({int((profit / capital) * 100)}%)')
